6e9ecd7bac8adca28add8184354acab0c42b9a9a
vasumathi298 <vasumathi.n29@gmail.com>
2023-05-29 00:26:52 -0400

    a3 changes

--- src/tuiter/who-to-follow-list/who-to-follow-list-item.js
+++ src/tuiter/who-to-follow-list/who-to-follow-list-item.js
@@ -9,7 +9,7 @@ const WhoToFollowListItem = (
   <li className="list-group-item">
    <div className="row">
      <div className="col-2">
-       <img className="rounded-circle" height={48} src={require(`../images/${who.avatarIcon}`)}/>
+       <img className="rounded-circle" height={48} alt="who avatar" src={require(`../images/${who.avatarIcon}`)}/>
      </div>
      <div className="col-8">
        <div className="fw-bold">{who.userName}</div>
