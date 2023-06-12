6b2114c955bdf6a31c88033d763fc8730a6c3ea2
vasumathi298 <vasumathi.n29@gmail.com>
2023-06-12 12:02:49 -0400

    make it responsive

--- src/tuiter/navigation-sidebar/index.js
+++ src/tuiter/navigation-sidebar/index.js
@@ -11,49 +11,49 @@ const NavigationSidebar = () => {
     <div className="list-group">
       <Link
         to="/tuiter/home"
-        className={`list-group-item ${active === "home" ? "active" : ""}`}
+        className={`list-group-item ${active === "home" ? "active" : ""} d-flex align-items-center`}
       >
         Home
       </Link>
       <Link
         to="/tuiter/explore"
-        className={`list-group-item ${active === "explore" ? "active" : ""}`}
+        className={`list-group-item ${active === "explore" ? "active" : ""} d-flex align-items-center`}
       >
         Explore
       </Link>
       <Link
         to="/tuiter/notifications"
-        className={`list-group-item ${active === "notifications" ? "active" : ""}`}
+        className={`list-group-item ${active === "notifications" ? "active" : ""} d-flex align-items-center`}
       >
         Notifications
       </Link>
       <Link
         to="/tuiter/messages"
-        className={`list-group-item ${active === "messages" ? "active" : ""}`}
+        className={`list-group-item ${active === "messages" ? "active" : ""} d-flex align-items-center`}
       >
         Messages
       </Link>
       <Link
         to="/tuiter/bookmarks"
-        className={`list-group-item ${active === "bookmarks" ? "active" : ""}`}
+        className={`list-group-item ${active === "bookmarks" ? "active" : ""} d-flex align-items-center`}
       >
         Bookmarks
       </Link>
       <Link
         to="/tuiter/lists"
-        className={`list-group-item ${active === "lists" ? "active" : ""}`}
+        className={`list-group-item ${active === "lists" ? "active" : ""}d-flex align-items-center`}
       >
         Lists
       </Link>
       <Link
         to="/tuiter/more"
-        className={`list-group-item ${active === "more" ? "active" : ""}`}
+        className={`list-group-item ${active === "more" ? "active" : ""} d-flex align-items-center`}
       >
         More
       </Link>
-      {!currentUser && <Link className={`list-group-item ${active === "login" ? "active" : ""}`} to="/tuiter/login">   Login   </Link>}
-     {!currentUser && <Link className={`list-group-item ${active === "register" ? "active" : ""}`} to="/tuiter/register">Register</Link>}
-     { currentUser && <Link className={`list-group-item ${active === "profile" ? "active" : ""}`} to="/tuiter/profile"> Profile </Link>}
+      {!currentUser && <Link className={`list-group-item ${active === "login" ? "active" : ""} d-flex align-items-center`} to="/tuiter/login">   Login   </Link>}
+     {!currentUser && <Link className={`list-group-item ${active === "register" ? "active" : ""} d-flex align-items-center`} to="/tuiter/register">Register</Link>}
+     { currentUser && <Link className={`list-group-item ${active === "profile" ? "active" : ""} d-flex align-items-center`} to="/tuiter/profile"> Profile </Link>}
     </div>
   );
 };
--- src/tuiter/who-to-follow-list/who-to-follow-list-item.js
+++ src/tuiter/who-to-follow-list/who-to-follow-list-item.js
@@ -8,14 +8,14 @@ const WhoToFollowListItem = (
  return(
   <li className="list-group-item">
    <div className="row">
-     <div className="col-2">
+   <div className="col-xl-2 col-lg-3">
        <img className="rounded-circle" height={48} alt="who avatar" src={require(`../images/${who.avatarIcon}`)}/>
      </div>
-     <div className="col-8">
+     <div className="col-xl-7 col-lg-5">
        <div className="fw-bold">{who.userName}</div>
        <div>@{who.handle}</div>
      </div>
-     <div className="col-2">
+     <div className="col-xl-3 col-lg-4 d-flex align-items-center justify-content-center">
        <button className="btn btn-primary rounded-pill float-end">Follow</button>
      </div>
    </div>
