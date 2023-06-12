590fd00754944b0463d64b2bc7f052038462204f
vasumathi298 <vasumathi.n29@gmail.com>
2023-06-12 00:36:15 -0400

    a5 changes

--- src/tuiter/services/tuits-service.js
+++ src/tuiter/services/tuits-service.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const TUITS_API = 'http://localhost:4000/api/tuits';   
+const TUITS_API = 'https://tuiter-node-server-app-z2lr.onrender.com/api/tuits';   
 export const findTuits = async () => {
     const response = await axios.get(TUITS_API);
     const tuits = response.data;
